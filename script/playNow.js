6cbf93ddaeb86bc7ae0ebbc91150ff5a199cb7b7
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-02-19 03:55:27 +0600

    get alphabet using index

--- script/playNow.js
+++ script/playNow.js
@@ -4,4 +4,12 @@ document.getElementById('playNow-btn').addEventListener('click', function(){
 
     let playGroundSection = document.getElementById('playGround');
     playGroundSection.classList.remove('hidden')
+
+    let alphabetString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let alphabets = alphabetString.split('');
+    
+    let randomNumber = Math.random()*25;
+    let index= Math.round(randomNumber);
+    let alphabet = alphabets[index];
+    console.log(alphabet);
 })
